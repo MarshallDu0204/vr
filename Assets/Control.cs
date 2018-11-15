0b59f2e3999fb536e92619be23e3129a1d76d399
MarshallDu0204 <2440075115@qq.com>
2018-11-15 19:24:33 +0800

    5.0

--- Assets/Control.cs
+++ Assets/Control.cs
@@ -4,42 +4,73 @@ using UnityEngine;
 using System;
 
 public class Control : MonoBehaviour {
-
+    AudioSource audioData;
+	bool permission = false;
 	// Use this for initialization
 	void Start () {
-		
+		audioData = GetComponent<AudioSource>();
 	}
+
+	public void give(){
+		permission = true;
+	}
+	
+	public void removePermission(){
+		permission = false;
+	}
+
+
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetAxis("Vertical")<0){
-			this.transform.Rotate(Vector3.forward*0.5f,Space.Self);
-		}
-		if (Input.GetAxis("Vertical")>0){
-			this.transform.Rotate(Vector3.back*0.5f,Space.Self);
-		}
-		if (Input.GetAxis("Horizontal")<0 && Input.GetAxis("Horizontal")>-0.95){
-			this.transform.Rotate(Vector3.down*0.5f,Space.Self);
-		}
-		if (Input.GetAxis("Horizontal")>0 && Input.GetAxis("Horizontal")<0.95){
-			this.transform.Rotate(Vector3.up*0.5f,Space.Self);
-		}
-		if (Input.GetAxis("Horizontal")>=0.95)
-            		{
-                		this.transform.Rotate(Vector3.left*0.5f,Space.Self);
-            		}
-		if (Input.GetAxis("Horizontal")<=-0.95)
-            		{
-                		this.transform.Rotate(Vector3.right*0.5f,Space.Self);
-            		}
-		if (Input.GetKey(KeyCode.Joystick1Button0))
-            		{
-                		this.transform.Translate(Vector3.right*0.1f,Space.Self);
-            		}
-		if (Input.GetKey(KeyCode.Joystick1Button2))
-            		{
-                		this.transform.Translate(Vector3.left*0.1f,Space.Self);
-            		}	
+		if (permission == true) {
+			if (Input.GetAxis("Vertical")<0){
+				this.transform.Rotate(Vector3.forward*0.5f,Space.Self);
+			}
+			if (Input.GetAxis("Vertical")>0){
+				this.transform.Rotate(Vector3.back*0.5f,Space.Self);
+			}
+			if (Input.GetAxis("Horizontal")<0 && Input.GetAxis("Horizontal")>-0.95){
+				this.transform.Rotate(Vector3.down*0.5f,Space.Self);
+			}
+			if (Input.GetAxis("Horizontal")>0 && Input.GetAxis("Horizontal")<0.95){
+				this.transform.Rotate(Vector3.up*0.5f,Space.Self);
+			}
+			if (Input.GetAxis("Horizontal")>=0.95)
+			{
+				this.transform.Rotate(Vector3.left*0.5f,Space.Self);
+			}
+			if (Input.GetAxis("Horizontal")<=-0.95)
+			{
+				this.transform.Rotate(Vector3.right*0.5f,Space.Self);
+			}
+			if (Input.GetKey(KeyCode.Joystick1Button0))
+			{
+				this.transform.Translate(Vector3.right*0.1f,Space.Self);
+			}
+			if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+			{
+				audioData.Play(0);
+			}
+			if (Input.GetKeyUp(KeyCode.Joystick1Button0))
+			{
+				audioData.Pause();
+			}
+			if (Input.GetKey(KeyCode.Joystick1Button2))
+			{
+				this.transform.Translate(Vector3.left*0.1f,Space.Self);						
+			}
+			if (Input.GetKeyDown(KeyCode.Joystick1Button2))
+			{
+				audioData.Play(0);
+			}
+			if (Input.GetKeyUp(KeyCode.Joystick1Button2))
+			{
+				audioData.Pause();
+			}	
 			
+		}
+		
 	}
+
 }
---
+++ Assets/test.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test : MonoBehaviour {
+	
+	public Image img;  
+	public float gazeTime = 0f;    
+	public float waitTime = 3f;     
+	public bool hasChangeMat = false;
+	public string name;
+	public GameObject mainoption1;
+	public GameObject mainoption2;
+	public GameObject mainoption3;
+  	//private Material otherMat;
+	public Control control;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = new Ray(this.transform.position, this.transform.forward);
+		RaycastHit hitInfo;
+		
+		if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity))
+        {
+            
+			name = hitInfo.collider.gameObject.name;
+			MeshRenderer otherRenderer = hitInfo.collider.gameObject.GetComponent<MeshRenderer>();
+
+		    if (otherRenderer != null)
+            {
+               
+			    if (!hasChangeMat && name == "mainoption3")  
+                {
+                   
+					gazeTime += Time.deltaTime;  
+						
+					 //otherMat = otherRenderer.material; 
+                
+			    }
+				else
+				{
+					gazeTime = 0f;
+					
+				}	 
+		    }
+            
+		    else
+			{
+                
+				name = "";
+            
+		    }
+ 
+        
+	    }
+        
+        else
+        {
+            
+			name = "";
+				
+			gazeTime = 0f;
+				
+			hasChangeMat = false;  
+        
+	    }
+ 
+        if (gazeTime >= waitTime)
+        {
+            gazeTime = 0f;
+            hasChangeMat = true;          
+			Destroy(mainoption1);
+			Destroy(mainoption2);
+			Destroy(mainoption3);
+			control.give();
+        }
+        else
+        {
+            img.fillAmount = 1 - (gazeTime / waitTime); 
+        }
+ 
+	}
+	
+}
